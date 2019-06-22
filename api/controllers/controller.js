7408363b78c601c3d826695e54e1efbeb6f817de
Samarth Patel <samarth.patel.22@hotmail.com>
2019-06-22 01:08:16 -0400

    Maps api works

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,7 +1,32 @@
 const googleMaps = require('@google/maps').createClient({
-    key: 'add key here'
+    key: 'AIzaSyARYSwyEt6vuwBLJ7X9H09l8N2yigdO3ME',
 });
 
 exports.test = function(req, res) {
-
+    googleMaps.places({
+        query: 'restaurants in Sydney',
+    }, function (err, response) {
+        if (!err) {
+            
+            res.send(response)
+        } else {
+            res.send(err)
+        }
+    })
 }
+
+// exports.teest = function(req, res) {
+//     googleMaps.findPlace({
+//         input: 'restaurants in Sydney',
+//         inputtype: 'textquery',
+//         fields: ['formatted_address']
+//     }, function (err, response) {
+//         if (!err) {
+//             //console.log(JSON.stringify(response));
+            
+//             res.send(response)
+//         } else {
+//             res.send(err)
+//         }
+//     })
+// }
--- api/server.js
+++ api/server.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 
 const routes = require("./routes/routes");
 
+app = express()
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false})) // Check what urlencoded does
- app.use('/api', routes)
+app.use('/api', routes)
 
 let port = 1111;
 
