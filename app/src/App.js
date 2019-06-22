a872a8fcb894b4a83cc5e0d9024fd6b206ee1d50
Samarth Patel <samarth.patel.22@hotmail.com>
2019-06-22 11:47:22 -0400

    Added bootstrap

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
+import './bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
+      <button className="btn-primary">Test</button>
     </div>
   );
 }
