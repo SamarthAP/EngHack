4ae1c7b9e41aefb5d4ea56a90863cec24dcf030b
Samarth Patel <samarth.patel.22@hotmail.com>
2019-06-21 21:50:12 -0400

    Created backend foundation

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const routes = require("./routes/routes");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false})) // Check what urlencoded does
+ app.use('/api', routes)
+
+let port = 1111;
+
+app.listen(port, () => {
+    console.log('Server running on port ' + port);
+})
